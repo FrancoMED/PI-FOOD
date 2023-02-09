74a6ef8c48183b2088e3cf1bb237df384d8623e4
FrancoMED <francoemedina@gmail.com>
2023-02-09 17:00:33 -0300

    deploy back railway

--- api/index.js
+++ api/index.js
@@ -21,6 +21,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { Diet } = require('./src/db.js');
 const { preload } = require('./src/controllers/Diets_C.js');
+require('dotenv').config();
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
@@ -28,7 +30,7 @@ conn.sync({ force: true }).then(async () => {
 	if (!aux.length) {
 		await preload();
 	}
-	server.listen(3001, () => {
-		console.log('%s listening at 3001'); // eslint-disable-line no-console
+	server.listen(PORT, () => {
+		console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
 	});
 });
